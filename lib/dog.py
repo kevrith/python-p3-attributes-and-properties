c71eb998ca847d6e9b491954518f9a79e668cff6
KELVIN <kevrith@gmail.com>
2025-11-17 19:03:12 +0300

    completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None, **kwargs):
+        self._name = None
+        self._breed = None
+        if name is not None:
+            self.name = name
+        elif 'name' in kwargs:
+            self.name = kwargs['name']
+        if breed is not None:
+            self.breed = breed
+        elif 'breed' in kwargs:
+            self.breed = kwargs['breed']
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 25:
+            self._name = new_name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, new_breed):
+        if new_breed in APPROVED_BREEDS:
+            self._breed = new_breed
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None, **kwargs):
+        self._name = None
+        self._job = None
+        if name is not None:
+            self.name = name
+        elif 'name' in kwargs:
+            self.name = kwargs['name']
+        if job is not None:
+            self.job = job
+        elif 'job' in kwargs:
+            self.job = kwargs['job']
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 25:
+            self._name = new_name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, new_job):
+        if new_job in APPROVED_JOBS:
+            self._job = new_job
+        else:
+            print("Job must be in list of approved jobs.")
